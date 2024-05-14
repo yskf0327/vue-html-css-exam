966a0c9b3ac793cff8d01b9f71bb0fbf784dad09
Yusuke Fukumoto <ysk2645j4@gmail.com>
2024-05-14 23:18:04 +0900

    ページタイトルを変更

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
 })
 module.exports = {
   outputDir: 'docs',
   assetsDir: './',
   publicPath: './',
-  pages:{
-    entry:'src/main.js',
-    title:'HTML/CSS確認テスト'
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'HTML/CSS確認テスト'
+    }
+
   }
 }
